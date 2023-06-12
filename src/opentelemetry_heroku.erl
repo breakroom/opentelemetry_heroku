3a39281a5570d424215848c223af6c2624855658
Tom Taylor <tom@tomtaylor.co.uk>
2023-06-12 15:54:36 +0100

    Only return environment variables which exist

--- src/opentelemetry_heroku.erl
+++ src/opentelemetry_heroku.erl
@@ -6,18 +6,23 @@
 -include_lib("opentelemetry_semantic_conventions/include/resource.hrl").
 
 get_resource(_) ->
-    otel_resource:create([
-        {?CLOUD_PROVIDER, "heroku"},
+    Attributes = [
+        {?CLOUD_PROVIDER, <<"heroku">>},
         {?SERVICE_NAME, get_env("HEROKU_APP_NAME")},
         {?SERVICE_INSTANCE_ID, get_env("HEROKU_DYNO_ID")},
         {?SERVICE_VERSION, get_env("HEROKU_RELEASE_VERSION")},
         {'heroku.app.id', get_env("HEROKU_APP_ID")},
         {'heroku.release.creation_timestamp', get_env("HEROKU_RELEASE_CREATED_AT")},
         {'heroku.release.commit', get_env("HEROKU_SLUG_COMMIT")}
-    ]).
+    ],
+
+    % Don't return environment variables that don't exist
+    CleanAttributes = lists:filter(fun({_Key, Value}) -> is_binary(Value) end, Attributes),
+
+    otel_resource:create(CleanAttributes).
 
 get_env(Var) ->
     case os:getenv(Var) of
-        false -> <<"unknown">>;
+        false -> false;
         Val -> unicode:characters_to_binary(Val)
     end.
